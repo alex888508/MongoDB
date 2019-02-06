31703be2e3100a3c344d894bd719e25c91a292ba
alex888508 <alex.korantiy@gmail.com>
2019-02-06 19:58:57 +0200

    CORS policy upgrade

--- src/main/java/com/mongodb/SimpleCORSfilter.java
+++ src/main/java/com/mongodb/SimpleCORSfilter.java
@@ -17,7 +17,7 @@ public class SimpleCORSfilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         chain.doFilter(req, res);
     }
 
